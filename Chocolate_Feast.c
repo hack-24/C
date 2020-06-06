56357a24cb981ce5e0597fd7ba403aae2a2f7b58
Navneet Ranjan <500067951@stu.upes.ac.in>
2020-06-06 18:37:52 +0530

    Chocolate feast Hackerrank

--- Chocolate_Feast.c
+++ Chocolate_Feast.c
@@ -1,61 +1,70 @@
-import java.util.*;
-import java.io.*;
+#include <stdio.h>
+#include <stdlib.h>
 
-
-
-class Solution{
-    public static void main(String []argh)
-    {
-
-
-
-        Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-
-        for(int i=0;i<t;i++)
-        {
-
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=Byte.MIN_VALUE && x<=Byte.MAX_VALUE){
-                 System.out.println("* byte");
-                  System.out.println("* short");
-                   System.out.println("* int");
-                    System.out.println("* long");
-                }
-
-                else if(x>=Short.MIN_VALUE && x<=Short.MAX_VALUE){
-
-
-                System.out.println("* short");
-                   System.out.println("* int");
-                    System.out.println("* long");
-
-                }
-
-
-                else if(x>=Integer.MIN_VALUE && x<=Integer.MAX_VALUE){
-
-                     System.out.println("* int");
-                    System.out.println("* long");
-
-                }
-
-
-                else 
-                System.out.println("* long");
+int main(int argc, char **argv)
+{
+    int t, total_Chocolates=0,total_Wrappers=0,temp=0;
+    
+    scanf("%d",&t);
+    int arr[t][3];
+    
+    
+    
+    //taking inputs inside the array
+    for(int i=0;i<t;i++){
+        
+        for(int j=0;j<3;j++){
+            
+            scanf("%d",&arr[i][j]);
+        }
+        
+    }
+    
+    for(int k=0;k<t;k++){
+        total_Chocolates=0;
+        total_Wrappers=0;
+        
+        for(int l=0;l<1;l++){
+            
+            total_Chocolates+=arr[k][l]/arr[k][l+1];
+             total_Wrappers+=total_Chocolates;
+            
+            //checking if the total_wrappers are greater than or equal to m
+           while(total_Wrappers>=arr[k][l+2]){
+               
+               
+               temp=total_Wrappers/arr[k][l+2];
+               total_Wrappers=total_Wrappers%(arr[k][l+2]);
+               
+               total_Chocolates+=temp;//incrementing total chocolates by temp
+               
+               total_Wrappers+=temp; //incrementing total wrappers by temp
+               
+               
+               
+               
+               
+               
+               
+               
+           }
+                
+                
+                
+                
+                
+                
+                
+                
+                
                 
             }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
-            }
-
+            
+            printf("%d\n",total_Chocolates);
+            
+            
         }
+        
+     return(0); 
+        
     }
-}
-
-
-
